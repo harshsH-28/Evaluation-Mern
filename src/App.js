420d2b672b4f3c0873b5e0a927e2d2dc413cd001
Harsh Sharma <harshsharma6419@gmail.com>
2022-10-31 16:52:50 +0530

    Deployed 1.0

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ function App() {
           Internship Assessment - Harsh Sharma
         </h1>
         <Home></Home>
+        <div className="flex justify-center items-center mt-4">
+          <p className="text-center m-2 mr-8 text-2xl">A -- 10</p>
+          <p className="text-center m-2 mr-8 text-2xl">B -- 50</p>
+          <p className="text-center m-2 mr-8 text-2xl">C -- 30</p>
+          <p className="text-center m-2 mr-8 text-2xl">D -- 5</p>
+          <p className="text-center m-2 mr-8 text-2xl">E -- 20</p>
+        </div>
       </div>
     </DndProvider>
   );
